17a2845b3ab4dbd8877fa6f6efdc5d4acdef5269
Ethan Phelps-Goodman <ethanpg@gmail.com>
2015-02-07 15:03:33 -0800

    allowing POST requests

--- sms/views.py
+++ sms/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 import twilio.twiml
 
 
+@csrf_exempt
 def hello(request):
     r = twilio.twiml.Response()
     r.message("Hello from Apartment Alerts.")
